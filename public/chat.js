9ad600fffcd51ab0ef54b2cbf259195037053ca3
Jacson Rai <59689244+Jacsonrai@users.noreply.github.com>
2020-06-10 12:31:09 +0545

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -3,7 +3,7 @@
 
 $(function(){
     //make connection
-    var socket=io.connect('https://localhost:3000')
+    var socket=io.connect('https://git.heroku.com/chat1996.git')
     //buttons and input
     var message=$("#message")
     var username=$("#username")
